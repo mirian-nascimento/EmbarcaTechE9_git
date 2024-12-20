1999c78ebd4dcf6f183d9ac0bb68eced8826dcca
Isaque4nd <andradeisq1.7@gmail.com>
2024-12-19 23:01:05 -0300

    definindo os fatores de conversao

--- conversor_armazenamento.c
+++ conversor_armazenamento.c
@@ -2,3 +2,9 @@
 #include <string.h>
 #include <math.h>
 
+// fatores de conversao
+#define BITS_IN_BYTE 8
+#define BYTES_IN_KB 1024
+#define KB_IN_MB 1024
+#define MB_IN_GB 1024
+#define GB_IN_TB 1024
